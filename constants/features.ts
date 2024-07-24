dfe8f109d1d3a4ad23194fc66217787a894f68b5
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-07-24 22:18:40 +0530

    clerk elements

---
+++ constants/features.ts
@@ -0,0 +1,35 @@
+import { Brain, Cloud, Computer, Eye, Globe, Zap } from "lucide-react";
+
+
+export const features = [
+    {
+        name: "Store all your PDF files",
+        description: "Keep all your files stored securely and accessible anywhere, anytime.",
+        icon: Globe
+    },
+    {
+        name: "Blazing fast responses",
+        description: "Get lightning fast responses to your queries ensuring instant gain of information",
+        icon: Zap
+    },
+    {
+        name: "chat memorization",
+        description: "Our chatbot remembers all previous interactions, providing seamless and personalized experience",
+        icon: Brain
+    },
+    {
+        name: "Interactive viewer",
+        description: "Engage with your documents with our interactive and intuitive viewer.",
+        icon: Eye
+    },
+    {
+        name: "Safe & Protected",
+        description: "Rest assured that all your documents are stored in backup safely",
+        icon: Cloud
+    },
+    {
+        name: "All devices",
+        description: "Access all you documents in all your devices be it desktop, laptop or mobile.",
+        icon: Computer
+    }
+]
